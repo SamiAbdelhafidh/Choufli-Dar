8aeb6199c46ed645bdb4ed6842c955044d6f6aa7
SamiAbdelhafidh <louzaysami@gmail.com>
2025-02-10 23:31:50 +0100

    views-admin-front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FrontLayoutComponent } from "./layouts/front-layout/front-layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FrontLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { LayoutsModule } from './layouts/layouts.module';
+import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:FrontLayoutComponent},
+    {path:'admin',component:AdminLayoutComponent}
+];
+
+@NgModule({
+    declarations:[
+        
+    ],
+    imports: [RouterModule.forRoot(routes),
+        FormsModule,
+        BrowserModule,
+        LayoutsModule,
+        AppComponent
+
+    ],
+    providers :[],
+    bootstrap:[]
+})
+export class AppModule { }
+
