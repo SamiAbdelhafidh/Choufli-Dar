455fea701fbaa5e44f22c1dc5d26036bce16e3aa
SamiAbdelhafidh <louzaysami@gmail.com>
2025-02-17 11:47:41 +0100

    routerLink loginuser-and-register

--- src/app/views/front/loginuser/loginuser/loginuser.component.ts
+++ src/app/views/front/loginuser/loginuser/loginuser.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-loginuser',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './loginuser.component.html',
   styleUrl: './loginuser.component.css'
 })
