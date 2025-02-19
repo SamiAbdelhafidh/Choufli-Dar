6cbf97801faeda9a18fb46068d022eba1c9d5b62
SamiAbdelhafidh <louzaysami@gmail.com>
2025-02-19 02:21:55 +0100

    edit

--- src/app/views/front/home/home/home.component.ts
+++ src/app/views/front/home/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @Component({
   selector: 'app-home',
-  imports: [
+  imports: [RouterModule
     
   ],
   templateUrl: './home.component.html',
