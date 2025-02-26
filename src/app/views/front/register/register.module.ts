b2c78281139733ca53614b2419695c0929d6aded
SamiAbdelhafidh <louzaysami@gmail.com>
2025-02-26 21:27:05 +0100

    error backend

--- src/app/views/front/register/register.module.ts
+++ src/app/views/front/register/register.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { RegisterRoutingModule } from './register-routing.module';
+import { RegisterComponent } from './register/register.component'; // Import RegisterComponent
+
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RegisterRoutingModule
+    RegisterRoutingModule,
+    FormsModule,
+    RegisterComponent
   ]
 })
 export class RegisterModule { }
--- src/app/views/front/register/register/register.component.ts
+++ src/app/views/front/register/register/register.component.ts
@@ -2,12 +2,16 @@ import { Component,OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AuthuserService } from '../../../services/authuser.service';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule,FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
+  standalone:true
 })
 export class RegisterComponent implements OnInit {
   constructor(private aus:AuthuserService) { }
