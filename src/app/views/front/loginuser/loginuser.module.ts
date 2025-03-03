f67b8a342076c5b09cd1c7cafacfd4cbc36a2784
SamiAbdelhafidh <louzaysami@gmail.com>
2025-03-04 00:44:16 +0100

    ngif error

--- src/app/views/front/loginuser/loginuser.module.ts
+++ src/app/views/front/loginuser/loginuser.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { LoginuserComponent } from './loginuser/loginuser.component';
 import { LoginuserRoutingModule } from './loginuser-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    LoginuserRoutingModule
+    FormsModule,
+    LoginuserRoutingModule,
+    LoginuserComponent
+    
   ]
 })
 export class LoginuserModule { }
--- src/app/views/front/loginuser/loginuser/loginuser.component.ts
+++ src/app/views/front/loginuser/loginuser/loginuser.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { AuthuserService } from '../../../services/authuser.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-loginuser',
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule,FormsModule],
   templateUrl: './loginuser.component.html',
-  styleUrl: './loginuser.component.css'
+  styleUrl: './loginuser.component.css',
+  standalone:true
 })
-export class LoginuserComponent {
-
+export class LoginuserComponent implements OnInit {
+   constructor(private aus:AuthuserService) { }
+  
+    ngOnInit(): void {    
+    }
+  
+    login(ff: any) {
+      let data=ff.value
+      this.aus.login(data).subscribe(data=>console.log(data))
+    }
 }
