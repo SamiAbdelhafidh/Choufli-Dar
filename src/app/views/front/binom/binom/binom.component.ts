3c1675923d946b19ab2d9a245e8dceeb9a883629
SamiAbdelhafidh <louzaysami@gmail.com>
2025-04-11 16:23:59 +0100

    page binom valide and +ajouter

--- src/app/views/front/binom/binom/binom.component.ts
+++ src/app/views/front/binom/binom/binom.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-binom',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './binom.component.html',
   styleUrl: './binom.component.css'
 })
 export class BinomComponent {
+  cards = [1, 2]; // Two default cards
 
+  addCard() {
+    this.cards.push(this.cards.length + 1);
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    alert('Form submitted!');
+  }
 }
