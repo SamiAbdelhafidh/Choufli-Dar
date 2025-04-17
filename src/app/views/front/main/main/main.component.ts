283d51226d74bb9ecaca25f6aa42bb7cf5839b9d
SamiAbdelhafidh <louzaysami@gmail.com>
2025-04-17 01:51:31 +0100

    image issue

--- src/app/views/front/main/main/main.component.ts
+++ src/app/views/front/main/main/main.component.ts
@@ -11,28 +11,28 @@ export class MainComponent {
   constructor(private router:Router) { }
   listings = [
     {
-      image: 'image1.jpg',
+      image: 'assets/images/image1.jpg',
       alt: 'Chambre spacieuse',
       line1: 'une chambre spacieuse dans s+2',
       line2: 'au centre ville proche de tous',
       price: '200dt'
     },
     {
-      image: 'image2.jpg',
+      image: 'assets/images/image2.jpg',
       alt: 'Chambre individuelle',
       line1: 'une chambre individuelle pour une personne dans s+3',
       line2: 'au centre ville',
       price: '190dt'
     },
     {
-      image: 'image3.jpg',
+      image: 'assets/images/image3.jpg',
       alt: 'Maison s+2 étudiants',
       line1: 'un maison s+2 pour les étudiants au centre proche de tous',
       line2: 'pour chaque\'un',
       price: '210dt'
     },
     {
-      image: 'image4.jpg',
+      image: 'assets/images/image4.jpg',
       alt: 'Maison s+3 étudiants',
       line1: 'un maison s+3 pour les étudiants',
       line2: 'pour chaque\'un',
