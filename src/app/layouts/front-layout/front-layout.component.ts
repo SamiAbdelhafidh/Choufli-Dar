d92032d67bc3f9c8acf58870e3843c977592ddf0
SamiAbdelhafidh <louzaysami@gmail.com>
2025-04-17 01:27:38 +0100

    lastt

--- src/app/layouts/front-layout/front-layout.component.ts
+++ src/app/layouts/front-layout/front-layout.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule, NavigationEnd } from '@angular/router';
-import { trigger, state, style, transition, animate } from '@angular/animations';
 import { filter, Subscription } from 'rxjs';
 
 @Component({
@@ -10,14 +9,7 @@ import { filter, Subscription } from 'rxjs';
   templateUrl: './front-layout.component.html',
   styleUrl: './front-layout.component.css',
   standalone: true,
-  animations: [
-    trigger('sidebarAnimation', [
-      state('closed', style({ transform: 'translateX(-100%)' })),
-      state('open', style({ transform: 'translateX(0)' })),
-      transition('closed => open', animate('300ms ease-in-out')),
-      transition('open => closed', animate('300ms ease-in-out'))
-    ])
-  ]
+
 })
 export class FrontLayoutComponent implements OnInit, OnDestroy {
   isSidebarOpen = false;
